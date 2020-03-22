ccfae88fe5643d2fe0193689f250a5569504b12e
Stephen P Potter, SE <56322084+spotter-puppet@users.noreply.github.com>
2020-03-21 22:21:44 -0400

    Update puppet-setup.sh

--- puppet-setup.sh
+++ puppet-setup.sh
@@ -3,7 +3,7 @@ rpm -Uvh https://yum.puppet.com/puppet-tools-release-el-7.noarch.rpm
 rpm -Uvh https://yum.puppet.com/puppet-release-el-7.noarch.rpm
 yum install -y puppet-bolt
 yum install -y pdk
-curl -JLO "https://pm.puppet.com/cgi-bin/download-cgi?dist=el&rel=7&arch=x86_64&ver=latest"
+curl -JLO "https://pm.puppet.com/cgi-bin/download.cgi?dist=el&rel=7&arch=x86_64&ver=latest"
 tar xvzf puppet-enterprise-2019.5.0-el-7-x86_64.tar.gz
 cd /root/puppet-enterprise-2019.5.0-el-7-x86_64
 ./puppet-enterprise-installer
